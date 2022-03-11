ab6eb15efa452538ddb1c0bceffc12b417af646e
Oguz <yilmazoguz@outlook.com>
2022-03-11 20:32:25 +0300

    Done.

--- src/features/todos/Todo.js
+++ src/features/todos/Todo.js
@@ -1,7 +1,21 @@
 import React from "react";
 
-function Todo({ text }) {
-  return <li>{text}</li>;
+import { useDispatch } from "react-redux";
+import { todoRemoved } from "./todosSlice";
+
+function Todo({ todo }) {
+  const dispatch = useDispatch();
+
+  function handleDeleteClick() {
+    dispatch(todoRemoved(todo.id));
+  }
+
+  return (
+    <li style={{marginBottom: 10}}>
+      <span>{todo.text}</span>
+      <button  onClick={handleDeleteClick} style={{marginLeft: 10, color:"black", backgroundColor: "red", borderRadius: 10}}>DELETE</button>
+    </li>
+  );
 }
 
 export default Todo;
